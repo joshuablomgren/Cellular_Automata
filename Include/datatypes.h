798374c255d7dcac200068b2e3ba474ac158e5d2
Jeffy Jeffy <jeffy_jeffy@berkeley.edu>
2022-12-09 18:19:36 -0600

    updated analysis and print functions

--- Include/datatypes.h
+++ Include/datatypes.h
@@ -12,6 +12,7 @@
 #include <vector>       // use the STL vector class for the grids
 #include <ctime>        // this program calls time() to initialize
                         // the seed of the C++/C random number generator
+#include <string>       // use the STL string class for filenames
 
 using namespace std;
 
@@ -25,6 +26,7 @@ class CellularAutomata
         int radius;      // Radius of the neighborhood
         int nstates;     // Number of states
         int rule;        // Rule
+        int dim1, dim2;  // Dimensions of the grid
 
         vector<vector<int>> current_grid;  // Current grid
         vector<vector<int>> next_grid;     // Next grid
@@ -37,7 +39,9 @@ class CellularAutomata
         int init_cond(int x_state, double prob);              // Set the initial condition of the cellular automata model
         int init_cond_rewrite(int x_state, double prob);      // rewrite for other cell states (not just empty)
         int setup_rule(int rule_type);                        // Set the rule of the cellular automata model
+        int grid_size();   // Size of the grid
         int step();                                           // Step the cellular automata model
-
-
+        int run_sim(int nsteps, bool print=true, bool file=false, string filename="none");  // Run the cellular automata model
+        int print_grid();                                     // Print the current grid
+        int print_grid(string filename);                        // Print the current grid to a file
 };
--- Source/CA_model/ca_model.cpp
+++ Source/CA_model/ca_model.cpp
@@ -290,7 +290,95 @@ int CellularAutomata::step(){
     }
     return 0;
 }
+
+// Function: Print the grid to the screen
+// Input: none
+// Output: none
+// Return: 0 (success)
+int CellularAutomata::print_grid() {
+    cout << "Current grid:" << endl;
+    cout << "-------------" << endl;
+    cout << "Grid size: " << current_grid.size() << " x " << current_grid[0].size() << endl;
+    for (int i = 0; i < current_grid.size(); i++) {
+        for (int j = 0; j < current_grid[i].size(); j++) {
+            cout << current_grid[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// Function: Print the grid to a file
+// Input: string filename
+// Output: none
+// Return: 0 (success)
+//         -1 (fail)
+int CellularAutomata::print_grid(string filename) {
+    ofstream outfile;
+    outfile.open(filename.c_str());
+    if (outfile.is_open()) {
+        outfile << "Current grid:" << endl;
+        outfile << "-------------" << endl;
+        outfile << "Grid size: " << current_grid.size() << " x " << current_grid[0].size() << endl;
+        for (int i = 0; i < current_grid.size(); i++) {
+            for (int j = 0; j < current_grid[i].size(); j++) {
+                outfile << current_grid[i][j] << " ";
+            }
+            outfile << endl;
+        }
+        outfile.close();
+        return 0;
+    } else {
+        cout << "Error: unable to open file" << endl;
+        return -1;
+    }
+}
                     
+// Function: Run the simulation
+// Perform the simulation for a given number of steps
+// Input: int number of steps
+//        bool whether to print the grid to the screen
+//        bool whether to print the grid to a file
+//        string filename
+// Output: none
+// Return: 0: success
+//         -1: fail (invalid number of steps)
+//         -2: fail (invalid boundary type)
+//         -3: fail (invalid neighborhood type)
+//         -4: fail (invalid filename)
+int CellularAutomata::run_sim(int steps, bool print_screen=true, bool print_file=false, string filename="none") {
+    int err;   // error code    
+
+    if (steps < 0) {
+        cout << "Error: invalid number of steps" << endl;
+        return (-1);   // Error: invalid number of steps
+    }
+
+    if(print_file == true && filename == "none") {
+        cout << "Error: invalid filename" << endl;
+        return (-4);   // Error: invalid filename
+    }
+
+    for (int i = 0; i < steps; i++) {
+        err = step(); 
+        if (err == -1) {
+            cout << "Error: invalid neighborhood type" << endl;
+            return (-3);   // Error: invalid neighborhood type
+        } else if (err == -2) {
+            cout << "Error: invalid boundary type" << endl;
+            return (-2);   // Error: invalid boundary type
+        }
+        if (print_screen == true) {
+            print_grid();
+        }
+
+        if (print_file == true) {
+            print_grid(filename);
+        }
+    }
+
+    return 0;
+}
 
     
 
