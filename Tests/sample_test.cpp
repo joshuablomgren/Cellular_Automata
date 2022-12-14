8376c4b22cc1b2520b3ff7549959386672b630e6
Jeffy Jeffy <jeffy_jeffy@berkeley.edu>
2022-12-13 22:37:36 -0600

    updated analysis files

--- Tests/sample_test.cpp
+++ Tests/sample_test.cpp
@@ -12,9 +12,10 @@ int main() {
     CellularAutomata CA;
     CA.setup_dimension(2, 10, 10);
     CA.setup_neighborhood(2);
-    CA.setup_boundtype(1, 1);
+    CA.setup_boundtype(0, 1);
     CA.setup_cell_states(2);
-    CA.init_cond(0, 0.5);
+    CA.init_cond(1, 0.5);
+    CA.init_cond_rewrite(2, 0.6);
     CA.setup_rule(1);
     CA.run_sim(10, true, false, "none");
     return 0;
